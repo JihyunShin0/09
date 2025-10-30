1ae584e40d361b351fe2879a0fabf4553363330f
JihyunShin0 <jirung@sookmyung.ac.kr>
2025-10-30 15:46:35 +0900

    09_02

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 5
+
 int main(int argc, char *argv[])
 {
-    //5명의 학생에 대한 시험성적 저장 배열 선언 
+    int i, average;
+    int sum;
+    int grade[SIZE];
     
-    int grade[5];
-    int i;
+    sum = 0;
     
-    grade[0] = 10;
-    grade[1] = 20;
-    grade[2] = 30;
-    grade[3] = 40;
-    grade[4] = 50;
+    for (i = 0 ; i < SIZE ; i++)
+    {
+        printf("점수를 입력하세요. \n");
+        scanf("%d", &grade[i]);
+        sum += grade[i];
+    }
+        
+    average = sum / SIZE;
+    printf("성적 평균 : %d\n", average); 
     
-    for ( i = 0 ; i < 5 ; i++ )
-        printf("grade[%d] = %d\n", i, grade[i]);
-  
+    #if 0
+    for ( i = 0 ; i < SIZE ; i++)
+        printf("grade[%i] = %i\n", i, grade[i]);
+    #endif
+    
     system("PAUSE");	
     return 0;
 }
