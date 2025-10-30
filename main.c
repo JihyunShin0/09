0de1235e0f4200ade87d27ee895df7f6f36856eb
JihyunShin0 <jirung@sookmyung.ac.kr>
2025-10-30 16:06:54 +0900

    09_03

--- main.c
+++ main.c
@@ -5,26 +5,45 @@
 
 int main(int argc, char *argv[])
 {
-    int i, average;
-    int sum;
-    int grade[SIZE];
-    
-    sum = 0;
+    int i;
+    int a[SIZE] = {1, 2, 3, 4, 5};
+    int b[SIZE] = {1, 2, 3, 4, 6};
+    int flag_same = 1;
     
     for (i = 0 ; i < SIZE ; i++)
     {
-        printf("점수를 입력하세요. \n");
-        scanf("%d", &grade[i]);
-        sum += grade[i];
+        if (a[i] != b[i])
+        {
+            printf("서로 다른 값을 가진 인덱스 : %d\n", i);
+            flag_same = 0;
+        }
     }
-        
-    average = sum / SIZE;
-    printf("성적 평균 : %d\n", average); 
+    
+    if ( flag_same )
+         printf("배열은 같은 값을 가집니다.\n");
+    else 
+         printf("배열은 다른 값을 가집니다.\n");
     
     #if 0
+    int grade[SIZE];
+    int score[SIZE]; 
+    
+    for (i = 0 ; i < SIZE ; i++)
+        grade[i] = rand() % 100;
+        
+    for (i = 0 ; i < SIZE ; i++)
+        score[i] = grade[i];
+    
     for ( i = 0 ; i < SIZE ; i++)
-        printf("grade[%i] = %i\n", i, grade[i]);
+        printf("grade[%i] = %i, score[%i] = %i\n", i, grade[i], i, score[i]);
+     
+     
+    if ( score == grade )
+         printf("배열은 같은 값을 가집니다.\n");
+    else 
+         printf("배열은 다른 값을 가집니다.\n");   
     #endif
+    
     
     system("PAUSE");	
     return 0;
